68d67649dad0558d965050d27ec0517328c3b776
m.zuev <m.zuev@azoft.com>
2021-11-14 18:54:51 +0100

    add fragments, resources

---
+++ app/src/main/java/dev/m13d/composition/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package dev.m13d.composition.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.m13d.composition.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
